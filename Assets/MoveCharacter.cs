2d45282a93daf9b56d2b26e8caf8d9b124515885
Juan Martin Gallo <juanmartingallo93@gmail.com>
2016-06-06 19:57:21 -0300

    los bloque

---
+++ Assets/MoveCharacter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCharacter : MonoBehaviour
+{
+
+    public float speed = 40f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + Camera.main.transform.forward * speed * Time.deltaTime;
+    }
+}
