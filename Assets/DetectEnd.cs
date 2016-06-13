45d356379964a273f0bf5e3cca530f3fa8c66652
Juan Martin Gallo <juanmartingallo93@gmail.com>
2016-06-13 20:29:52 -0300

    lab finished

---
+++ Assets/DetectEnd.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectEnd : MonoBehaviour {
+
+    public GameObject cartel;
+    public MoveCharacter move;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.name == "CardboardMain")
+        {
+            cartel.SetActive(true);
+            move.Finish();
+        }
+    }
+}
--- Assets/MoveCharacter.cs
+++ Assets/MoveCharacter.cs
@@ -6,6 +6,7 @@ public class MoveCharacter : MonoBehaviour
 
     public float speed = 40f;
     public bool walkEnabled = true;
+    public bool finish = false;
 
     // Use this for initialization
     void Start()
@@ -16,18 +17,21 @@ public class MoveCharacter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*transform.position = transform.position + Camera.main.transform.forward * speed * Time.deltaTime;*/
+        
 
-        if (walkEnabled)
+        if (!finish && walkEnabled)
         {
-            CharacterController controller = GetComponent<CharacterController>();
+            Vector3 pos = transform.position + Camera.main.transform.forward * speed * Time.deltaTime;
+            pos.y = 23;
+            transform.position = pos;
+           /* CharacterController controller = GetComponent<CharacterController>();
             Vector3 moveDirection = new Vector3();
             if (controller.isGrounded)
             {
                 moveDirection = Camera.main.transform.forward;
                 moveDirection *= speed;
             }
-            controller.Move(moveDirection * Time.deltaTime);
+            controller.Move(moveDirection * Time.deltaTime);*/
         }
     }
 
@@ -36,6 +40,9 @@ public class MoveCharacter : MonoBehaviour
         walkEnabled = !walkEnabled;
     }
 
-
+    public void Finish()
+    {
+        finish = true;
+    }
 
 }
