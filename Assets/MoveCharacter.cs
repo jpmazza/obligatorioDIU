37899b6187b26b0f9d502f374321802684a2656a
Juan Martin Gallo <juanmartingallo93@gmail.com>
2016-06-13 18:50:34 -0300

    lab

--- Assets/MoveCharacter.cs
+++ Assets/MoveCharacter.cs
@@ -5,6 +5,7 @@ public class MoveCharacter : MonoBehaviour
 {
 
     public float speed = 40f;
+    public bool walkEnabled = true;
 
     // Use this for initialization
     void Start()
@@ -15,6 +16,26 @@ public class MoveCharacter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + Camera.main.transform.forward * speed * Time.deltaTime;
+        /*transform.position = transform.position + Camera.main.transform.forward * speed * Time.deltaTime;*/
+
+        if (walkEnabled)
+        {
+            CharacterController controller = GetComponent<CharacterController>();
+            Vector3 moveDirection = new Vector3();
+            if (controller.isGrounded)
+            {
+                moveDirection = Camera.main.transform.forward;
+                moveDirection *= speed;
+            }
+            controller.Move(moveDirection * Time.deltaTime);
+        }
     }
+
+    public void ChangeWalkEnabled()
+    {
+        walkEnabled = !walkEnabled;
+    }
+
+
+
 }
